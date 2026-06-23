72c56e279601136177f5ca6e42800f60257e702e
SRISHTI AGGARWAL <srishtiaggarwal1303@gmail.com>
2026-06-23 15:41:14 +0530

    Add files via upload

---
+++ DSA IN JAVA/Basic/VariableBasics.java
@@ -0,0 +1,11 @@
+package Basic;
+
+public class VariableBasics {
+    public static void main(String[] args) {
+        int a=23;
+        System.out.println(a+2);
+        int b; //declaration
+        b=10;  //initialization
+        System.out.println(b-5);
+    }
+}
